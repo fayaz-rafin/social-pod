063a8c8718fcc77e4216626ea2cb27d906b40234
fayaz-rafin <fayaz.rafin@gmail.com>
2025-06-28 13:28:40 -0400

    feat: make the app into a PWA

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    
 };
 
-export default nextConfig;
+const withPWA = require('next-pwa')({
+    dest: 'public'
+})
+
+module.exports = withPWA(nextConfig);
